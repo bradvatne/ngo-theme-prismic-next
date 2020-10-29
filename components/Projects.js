e735dc053befe50c7f5bc4bd6b35fdb1ba7defb6
Brad Vatne <brad.vatne@gmail.com>
2020-10-29 13:48:01 +0800

    carousel working i think

--- components/Projects.js
+++ components/Projects.js
@@ -17,53 +17,22 @@ const Projects = ({ projects }) => {
         <RichText render={projects.primary.projects_description} />
       </div>
       <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block h-100 w-100 img-fluid"
-            src={projects.items[0].project_image.url}
-            alt="Slide"
-          />
-          <Carousel.Caption>
-            <RichText render={projects.items[0].project_title} />
-            <RichText render={projects.items[0].project_description} />
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block h-100 w-100"
-            src={projects.items[1].project_image.url}
-            alt="Slide"
-          />
-          <Carousel.Caption>
-            <RichText render={projects.items[1].project_title} />
-            <RichText render={projects.items[1].project_description} />
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block h-100 w-100"
-            src={projects.items[1].project_image.url}
-            alt="Slide"
-          />
-          <Carousel.Caption>
-            <RichText render={projects.items[1].project_title} />
-            <RichText render={projects.items[1].project_description} />
-          </Carousel.Caption>
-        </Carousel.Item>
+        {projects.items.map((item) => (
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={item.project_image.url}
+              alt="Slide"
+            />
+            <Carousel.Caption>
+              <RichText render={item.project_title} />
+              <RichText render={item.projects_description} />
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </section>
   );
 };
-
-const RenderCarouselItems = ({ items }) =>
-  items.map((item, key) => (
-    <Carousel.Item key={key}>
-      <img className="d-block w-100" src={item.project_image.url} alt="Slide" />
-      <Carousel.Caption>
-        <RichText render={item.project_title} />
-        <RichText render={item.projects_description} />
-      </Carousel.Caption>
-    </Carousel.Item>
-  ));
 
 export default Projects;

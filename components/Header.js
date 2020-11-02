d97b58f046e47eef30bf2bf6239e952d0da03460
Brad Vatne <brad.vatne@gmail.com>
2020-11-02 18:35:12 +0800

    navbar sticky on scroll added

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,33 @@
 import { RichText } from "prismic-reactjs";
 import { Navbar, Nav } from "react-bootstrap";
+import { useState } from "react";
 import Link from "next/link";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 const Header = ({ header }) => {
+  const [stickyNav, setStickyNav] = useState(false);
+  useScrollPosition(
+    ({ currPos }) => {
+      const scrollY = Math.abs(currPos.y);
+      if (scrollY > 900 && !stickyNav) setStickyNav(true);
+      if (scrollY < 100 && stickyNav) setStickyNav(false);
+    },
+    [stickyNav]
+  );
+
   return (
     <Navbar
-      bg="dark"
-      variant="dark"
       expand="md"
-      className="position-absolute w-100 bg-transparent"
+      className={
+        stickyNav
+          ? "fixed-top w-100 bg-primary"
+          : "position-absolute w-100 bg-transparent"
+      }
     >
       <Navbar.Brand>
-        <Link href="/"><a className="text-white">{RichText.asText(header.brand_text)}</a></Link>
+        <Link href="/">
+          <a className="text-white">{RichText.asText(header.brand_text)}</a>
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">

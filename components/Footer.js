d95770b689e57d39b240cdb7fc8c3e145f6d3b83
Brad Vatne <brad.vatne@gmail.com>
2020-11-03 10:52:38 +0800

    navbar work

--- components/Footer.js
+++ components/Footer.js
@@ -1,33 +1,57 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Button, Row, Form, Col, Container } from "react-bootstrap";
+import { RichText } from "prismic-reactjs";
 import Link from "next/link";
 
 const Footer = ({ footer, nav }) => {
-  console.log("footer test");
-  console.log(footer);
   return (
-    <div className="bg-primary text-light">
+    <section className="section-footer bg-primary text-light pt-3">
       <Container>
-        <Row className="py-5"></Row>
+        <Row className="d-flex justify-content-between">
+          <Col md="6">
+            <RichText render={footer.brand_name} />
+            <RichText render={footer.brand_description} />
+          </Col>
+          <Col md="3">
+            <RichText render={footer.email_capture_title} />
+            <Form>
+              <Form.Control className="my-2" placeholder="Email Address" />
+              <Form.Control className="my-2" placeholder="Name" />
+              <Button className="my-2" className="btn-outline-light w-100">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
         <SocialsWidget socials={footer} />
       </Container>
-    </div>
+    </section>
   );
 };
 
 const SocialsWidget = ({ socials }) => (
-  <Row className="d-flex justify-content-center pb-3">
+  <Row className="d-flex justify-content-center py-3">
     {socials.socials_widget.map((item, index) => (
-        <Link href="/placeholder" className="text-light">
-          <a className="text-light px-2">
-            <i
-              className={
-                "fa-2x fab fa-" + item.socials_widget_icon.toLowerCase()
-              }
-            />
-          </a>
-        </Link>
+      <Link href="/placeholder" className="text-light">
+        <a className="text-light px-2">
+          <i
+            className={"fa-2x fab fa-" + item.socials_widget_icon.toLowerCase()}
+          />
+        </a>
+      </Link>
     ))}
   </Row>
 );
+
+const NavLinks = ({ navList }) => {
+  return (
+    <div className="d-flex flex-column">
+      {navList.map((navItem, index) => (
+        <Link href={navItem.link.type} key={index}>
+          <a className="text-white">{RichText.asText(navItem.label)}</a>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Footer;
--- components/Header.js
+++ components/Header.js
@@ -6,53 +6,66 @@ import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 import Fade from "react-bootstrap/Fade";
 
 const Header = ({ header }) => {
-  const [stickyNav, setStickyNav] = useState(false);
-  useScrollPosition(
-    ({ currPos }) => {
-      const scrollY = Math.abs(currPos.y);
-      if (scrollY > 900 && !stickyNav) setStickyNav(true);
-      if (scrollY < 200 && stickyNav) setStickyNav(false);
-    },
-    [stickyNav]
-  );
+  const [stickyNav, setStickyNav] = useState({
+    render: false,
+    animate: false,
+  });
+  useScrollPosition(({ currPos }) => {
+    const scrollY = Math.abs(currPos.y);
+    if (scrollY > 300 && !stickyNav.render)
+      setStickyNav({
+        render: true,
+        animate: false,
+      });
+    if (scrollY > 800 && !stickyNav.render)
+      setStickyNav({
+        render: true,
+        animate: true,
+      });
+    if (scrollY < 299 && stickyNav.render)
+      setStickyNav({
+        render: false,
+        animate: false,
+      });
+  });
 
-  //refactor navbar as a variable and pass class as props
-  return (
-    <div className="nav-container">
-      <Navbar expand="md" className={"position-absolute w-100 bg-transparent"}>
-        <Navbar.Brand>
-          <Link href="/">
-            <a className="text-white">{RichText.asText(header.brand_text)}</a>
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <NavLinks navList={header.nav_item} />
-        </Navbar.Collapse>
-      </Navbar>
-      <Fade in={stickyNav}>
-        <Navbar expand="md" className="fixed-top w-100 bg-primary">
-          <Navbar.Brand>
-            <Link href="/">
-              <a className="text-white">{RichText.asText(header.brand_text)}</a>
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <NavLinks navList={header.nav_item} />
-          </Navbar.Collapse>
-        </Navbar>
+  const stickyClass = "fixed-top w-100 bg-primary";
+  const normalClass = "position-absolute w-100 bg-transparent";
+  const toggleId = "basic-navbar-nav";
+
+  //render = render the component
+  //animate = trigger fade in animation, making the compononent visible
+
+  if (!stickyNav.render && !stickyNav.animate)
+    return <NavBar header={header} navType={normalClass} />;
+  if (stickyNav.render)
+    return (
+      <Fade in={stickyNav.animate}>
+        <NavBar header={header} navType={stickyClass} />
       </Fade>
-    </div>
-  );
+    );
 };
+
+const NavBar = ({ header, navType, toggleId }) => (
+  <Navbar expand="md" className={navType}>
+    <Navbar.Brand>
+      <Link href="/">
+        <a className="text-white">{RichText.asText(header.brand_text)}</a>
+      </Link>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls={toggleId} />
+    <Navbar.Collapse id={toggleId}>
+      <NavLinks navList={header.nav_item} />
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 const NavLinks = ({ navList }) => {
   return (
     <Nav className="ml-auto">
       {navList.map((navItem, index) => (
         <Link href={navItem.link.type} key={index}>
-          <a className="text-white p-3">{RichText.asText(navItem.label)}</a>
+          <a className="text-white p-2">{RichText.asText(navItem.label)}</a>
         </Link>
       ))}
     </Nav>

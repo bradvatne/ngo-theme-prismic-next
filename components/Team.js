6b51272004c6501047a186d6cc97b16d4a3ce768
Brad Vatne <brad.vatne@gmail.com>
2020-11-09 13:22:57 +0800

    animations

--- components/Team.js
+++ components/Team.js
@@ -1,5 +1,6 @@
 import { Row, Col, Container } from "react-bootstrap";
 import { RichText } from "prismic-reactjs";
+import { Fade } from "react-awesome-reveal";
 
 export const Team = ({ team }) => {
   return (
@@ -7,14 +8,16 @@ export const Team = ({ team }) => {
       <Row className="d-flex flex-row justify-content-center">
         {team.items.map((item, index) => (
           <Col lg={4}>
-            <div className="text-center">
+            <Fade triggerOnce key={index} delay={index * 250}>
+              <div className="text-center">
                 <img
                   src={item.portrait.url}
                   className="img-fluid rounded-circle shadow-lg w-75 "
                 />
-              <RichText render={item.name} />
-            </div>
-            <RichText render={item.bio} />
+                <RichText render={item.name} />
+              </div>
+              <RichText render={item.bio} />
+            </Fade>
           </Col>
         ))}
       </Row>
